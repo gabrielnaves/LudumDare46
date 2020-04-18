7e95312dd39f0b24970b5b31a2eec1d1cb47377c
Gabriel Naves <gabri.navess@gmail.com>
2020-04-18 04:18:55 -0300

    Initial commit: empty project with my imported utilities folder

---
+++ Assets/Utility/Animation/AnimatorExtensions.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+static public class AnimatorExtensions {
+
+    static public bool IsCurrentState(this Animator animator, string name) {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(name);
+    }
+}
---
+++ Assets/Utility/UI/LayoutGroupAndContentSizeFitterEnabler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * Layout groups and content size fitter components tend to make a scene dirty all the time,
+ * which is annoying in source control. This is a known issue marked as "won't fix" in unity
+ * issues, so the workaround is to disable them and re-enable on awake.
+ */
+public class LayoutGroupAndContentSizeFitterEnabler : MonoBehaviour {
+
+    void Awake() {
+        GetComponent<LayoutGroup>().enabled = true;
+        GetComponent<ContentSizeFitter>().enabled = true;
+    }
+}
