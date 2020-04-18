05fc62af6001895087968a96940a31f910885409
Gabriel Naves <gabri.navess@gmail.com>
2020-04-18 07:41:28 -0300

    Started modeling the arm/hand, and added a bad camera follow script, will fix later

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float height;
+
+    void LateUpdate() {
+        if (target) {
+            transform.position = target.position + target.up * height;
+            Debug.Log(target.rotation.eulerAngles);
+        }
+    }
+}
